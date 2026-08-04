f89d0dd490795d8646100a15840c6e8295009ba9
nikitakochalkar2005-tech <nikitakochalkar2005@gmail.com>
2026-08-04 14:48:37 +0530

    add to_print_ASCII_value program to display the ASCII value of a character

---
+++ practice/programiz/to_print_ASCII_value.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main() {
+    char  c;
+    printf("enter a  character:");
+    scanf("%c",&c);
+    // %d displays the integer value of a character
+    // %c displays the actual character
+    printf("ASCII Value of %c= %d",c,c);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
