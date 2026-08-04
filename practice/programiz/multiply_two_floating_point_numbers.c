d3b88a2fb1a616fbbaaf7364a6ea2ab51661cccd
nikitakochalkar2005-tech <nikitakochalkar2005@gmail.com>
2026-08-04 21:00:30 +0530

    refactor: improve multiply_float function and enhance main program output

--- practice/programiz/multiply_two_floating_point_numbers.c
+++ practice/programiz/multiply_two_floating_point_numbers.c
@@ -4,17 +4,20 @@
  */
 float multiply_float(float first_number, float second_number)
 {
-     float product = first_number*second_number;
-     printf("multiplication of two folating poin is %f",product);
+    float product = first_number * second_number;
+    return product;
 }
 
 /**
  * c program to multiply two floating point numbers
  */
 int main(int argc, char const *argv[])
-{  
-//    printf("%d",23);
-    multiply_float(89.6, 3.5);
+{
+    //    printf("%d",23);
+
+    float product = multiply_float(89.6, 3.5);
+    printf("product of two floating point numbers is: %.3f", product);
+
     /* code */
     return 0;
 }
