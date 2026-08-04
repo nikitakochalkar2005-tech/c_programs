9c7a16628770bfb3a5d5640d5129ec4af3ca896a
nikitakochalkar2005-tech <nikitakochalkar2005@gmail.com>
2026-08-04 20:28:20 +0530

    add program to multiply two integers and display the result

---
+++ practice/programiz/multiply_two_floating_point_numbers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,multiplication;
+    printf("Enter two numbers:");
+    scanf("%d %d",&a,&b);
+    multiplication = a*b;
+    printf("multiplication of two numbers is:%d",multiplication);
+
+}
