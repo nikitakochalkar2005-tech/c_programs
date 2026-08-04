28fb59e40355e6a0720fee796b9ec4652906ccaf
nikitakochalkar2005-tech <nikitakochalkar2005@gmail.com>
2026-08-04 20:06:52 +0530

    refactor: update add_integers function for clarity and structure

--- practice/programiz/add_a_two_integer.c
+++ practice/programiz/add_a_two_integer.c
@@ -1,8 +1,20 @@
+#include <stdio.h>
+
+/**
+ * this function is used to add two integers
+ */
+void add_integers(int integer1, int integer2)
+{
+     
+    int integer_sum = integer1 + integer2;
+     printf("The sum of %d and %d is: %d\n", integer1, integer2, integer_sum);
+}
+
 /**
  *program to add two integers
  */
-int main(void)
-{
-
-    return 0;
+int main(void){
+// {   printf("%d",5);
+    add_integers(20,30);
+     return 0;
 }
