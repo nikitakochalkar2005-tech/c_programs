d673c154c8cac6e6cc3c759f6985495fd9ae5281
nikitakochalkar2005-tech <nikitakochalkar2005@gmail.com>
2026-08-04 13:52:28 +0530

    add add_a_two_integer.c to calculate and display the sum of two integers

---
+++ practice/programiz/add_a_two_integer.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int a,b ,sum;
+
+    
+    scanf("%d,%d",&a,&b);
+    sum=a+b;
+    printf("the sum two intger is:%d",sum);
+    return 0;
+}
+// #include <stdio.h>
+// int main() {    
+
+//     int number1, number2, sum;
+    
+//     printf("Enter two integers: ");
+//     scanf("%d %d", &number1, &number2);
+
+//     // calculate the sum
+//     sum = number1 + number2;      
+    
+//     printf("%d + %d = %d", number1, number2, sum);
+//     return 0;
+// }
