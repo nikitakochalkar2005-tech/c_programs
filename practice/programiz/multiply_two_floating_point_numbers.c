895c6cf2184c4d001b690bbcad6998dcd9ad9e89
nikitakochalkar2005-tech <nikitakochalkar2005@gmail.com>
2026-08-04 20:49:16 +0530

    refactor: fix formatting in multiply_float function output

--- practice/programiz/multiply_two_floating_point_numbers.c
+++ practice/programiz/multiply_two_floating_point_numbers.c
@@ -1,17 +1,20 @@
+#include <stdio.h>
 /**
  * this function is used to multiply two floating point numbers
  */
 float multiply_float(float first_number, float second_number)
 {
      float product = first_number*second_number;
-     
+     printf("multiplication of two folating poin is %f",product);
 }
 
 /**
  * c program to multiply two floating point numbers
  */
 int main(int argc, char const *argv[])
-{
+{  
+//    printf("%d",23);
+    multiply_float(89.6, 3.5);
     /* code */
     return 0;
 }
