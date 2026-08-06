50efef9ba8716f3e3b24a9ad7c2aef45767c8f3e
nikitakochalkar2005-tech <nikitakochalkar2005@gmail.com>
2026-08-06 12:16:22 +0530

    refactor: restructure compute_quotient_and_remainder program and add function for quotient and remainder calculation

--- practice/programiz/compute_quotient_and_remainder.c
+++ practice/programiz/compute_quotient_and_remainder.c
@@ -1,15 +1,37 @@
-#include<stdio.h>
-int main(){
-    int dividend,divisor,remainder,quotient;
+#include <stdio.h>
 
-    printf("enter the dividend :");
-    scanf("%d",&dividend);
-    printf("enter the divisor :");
-    scanf("%d",&divisor);
-    remainder = dividend%divisor;
-    quotient = dividend/divisor;
-    printf("the quotient is:%d\n",quotient);
+/**
+ * @brief this function returns the quotient and remainder of two numbers
+ * @return the quotient and remainder of two numbers
+ * @param two integers 
+ */
+ 
+int quotient_remainder(int quotient, int remainder)
+{
+   
+    return (int)quotient, remainder;
+}
 
-    printf("the remainder is:%d\n",remainder);
 
+
+
+
+
+/**
+ * 
+ * @brief this is the main function
+ * @return int 0
+ */
+
+int main(int argc, char const *argv[])
+{
+   int number1 = argv[1];
+   int number2 = argv[2];
+   int quotient = number1 / number2;
+   int remainder = number1 % number2;
+   
+
+   printf("the value of quotient is: %d\n the value of remainder is %d\n", quotient, remainder);
+   
+    return 0;
 }
