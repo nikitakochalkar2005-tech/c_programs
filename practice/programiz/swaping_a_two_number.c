df264cba3ec33047e44074323b4749123f5a587a
nikitakochalkar2005-tech <nikitakochalkar2005@gmail.com>
2026-08-08 20:13:49 +0530

    refactor: formate kiya hai file ko

--- practice/programiz/swaping_a_two_number.c
+++ practice/programiz/swaping_a_two_number.c
@@ -1,36 +1,27 @@
-#include<stdlib.h>
-#include<stdio.h>
-
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * @brief this function return integer value swaping the number 
+ * @brief this function return integer value swaping the number
  * @return integer
- * @param int 
+ * @param int
  */
 
-
-
-int main(int num1 , char const *argv[] )
+int main(int num1, char const *argv[])
 {
-  if (num1<1)
+  if (num1 < 1)
   {
     int void_number = num1;
-    printf("%i\n",void_number);
+    printf("%i\n", void_number);
   }
-  //int void_number = num1;
-  
+  // int void_number = num1;
+
   int swaping_number = atoi(argv[1]);
-  int  swaping_number2 = atoi(argv[2]);
-   int temp = swaping_number2;
-   swaping_number2 = swaping_number ;
-    printf("%i\n",temp);
-    printf("%i\n",swaping_number);
-  
+  int swaping_number2 = atoi(argv[2]);
+  int temp = swaping_number2;
+  swaping_number2 = swaping_number;
+  printf("%i\n", temp);
+  printf("%i\n", swaping_number);
+
   return 0;
 }
-
-
-
-
-
-
