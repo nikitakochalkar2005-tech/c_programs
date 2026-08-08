49264a922d8295aa198b4f94c08603bc3b735f35
nikitakochalkar2005-tech <nikitakochalkar2005@gmail.com>
2026-08-08 16:27:37 +0530

    feat: add program to determine if a number is even or odd

---
+++ practice/programiz/even_or_odd_number.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * @brief this function find the number is even or odd return int value 
+ * 
+ * @return int value 
+ * @param int number
+ */
+
+
+int main(int argc, char const *argv[])
+{     if (argc<1){
+
+      int num  = argc ;
+     printf("%i\n",num);
+    }
+    
+    int  num1 = atoi(argv[1]);
+     if (num1%2 == 0){
+      printf("number is even :%i\n",num1);  
+     }
+    else{
+    printf("number is odd :%i\n",num1);
+    }
+
+    return 0;
+}
