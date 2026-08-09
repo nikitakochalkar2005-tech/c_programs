83da7c9625e68f6e0fde197648bcfaf137b3e6e9
nikitakochalkar2005-tech <nikitakochalkar2005@gmail.com>
2026-08-09 15:59:41 +0530

    feat: add multiplication logic for two floating point numbers

--- java/MultiplyTwoFloatingNumbers.java
+++ java/MultiplyTwoFloatingNumbers.java
@@ -2,6 +2,10 @@
 
 public class MultiplyTwoFloatingNumbers {
     public static void main(String[] args) {
+        float a = 4.5f;
+        float b = 6.5f ;
+        float product = (a*b);
+        System.out.printf("%.2f\n",product);
         
     }
     
