b7bf6e014ff104d9bd8e76ad82dfc3cba22411fc
nikitakochalkar2005-tech <nikitakochalkar2005@gmail.com>
2026-09-08 22:12:40 +0530

    feat: add program to generate multiplication table for a given number

---
+++ practice/programiz/multiplication_table.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * @brief this function generate the multiplication table
+ * @param number the number for which to generate the multiplication table
+ * @return void function par operation karana hai
+ */
+void multiplication_table(int number)
+{
+
+    for (int number_multiply = 1; number_multiply <= 10; number_multiply++)
+    {
+
+        int product = number * number_multiply;
+        printf("%i\n", product);
+    }
+}
+
+int main(void)
+{
+    int number;
+    printf("Enter a number to generate its multiplication table: ");
+    scanf("%d", &number);
+    multiplication_table(number);
+    return 0;
+}
