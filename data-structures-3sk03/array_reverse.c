1cde39aa0f87335557fe00d0a8d2a5327dbd1b6d
nikitakochalkar2005-tech <nikitakochalkar2005@gmail.com>
2026-09-09 23:35:44 +0530

    feat: add program to reverse the elements of an array

---
+++ data-structures-3sk03/array_reverse.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * @brief this function reverse the array elements
+ * @param array konte array ko reverse karana hai
+ */
+void reverse_array_element(int array[], int size)
+{
+    for (int index = 0, end = size - 1; index < end; index++, end--)
+    {
+
+        array[index] = array[index] + array[end];
+        array[end] = array[index] - array[end];
+        array[index] = array[index] - array[end];
+        // printf("%i\n", array[index]);
+        //  printf("%i\n", array[3]);
+    }
+}
+
+int main(void)
+{
+    int size = 4;
+    int array[] = {1, 7, 78, 9};
+
+    reverse_array_element(array, size);
+    for (int i = 0; i < size; i++)
+    {
+
+        printf("%i\n", array[i]);
+    }
+    return 0;
+}
