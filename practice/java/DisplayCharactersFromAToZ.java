c0e979c4a2b5754f8fee447dde5374db3cbecbd1
nikitakochalkar2005-tech <nikitakochalkar2005@gmail.com>
2026-09-12 14:24:51 +0530

    feat: add program to display characters from A to Z

---
+++ practice/java/DisplayCharactersFromAToZ.java
@@ -0,0 +1,13 @@
+class DisplayCharactersFromAToZ{
+    static void DisplayCharacter(){
+    for(char character = 'A'; character <= 'Z'; character++){
+        //  System.out.print("   " character+);
+         System.out.print(character + " ");
+
+        }
+        System.out.println();
+    }
+    public static void main(String args[]){
+        DisplayCharacter();
+    }
+}
