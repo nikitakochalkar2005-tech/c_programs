1f8a35df9193df518fed5199f0e61991b95da723
nikitakochalkar2005-tech <nikitakochalkar2005@gmail.com>
2026-09-18 14:05:02 +0530

    feat: add function to display factors of a number

---
+++ practice/programiz/display_factors_of_number.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/***
+ * @brief this function display factors of a number
+ * @param number : parameter number to find factors
+ */
+void display_factors_of_number(int number)
+{
+    for (int divisor = 1; divisor <= number; divisor++)
+    {
+        if (number % divisor == 0)
+        {
+            printf("it a factor of number:%i\n", divisor);
+        }
+    }
+}
+
+int main(void)
+{
+    int number;
+    printf("enter a number to find factors:");
+    scanf("%d", &number);
+    display_factors_of_number(number);
+
+    return 0;
+}
