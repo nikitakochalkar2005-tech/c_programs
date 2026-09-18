b1485c11e28d1c6c821ef7d4bc417550fe6a40fc
nikitakochalkar2005-tech <nikitakochalkar2005@gmail.com>
2026-09-18 14:29:45 +0530

    feat: add DisplayFactorsOfNumber class to display factors of a number

---
+++ practice/java/DisplayFactorsOfNumber.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+/**
+ *
+ * @brief this function display factors of a number
+ * @param number: parameter number isliye liya ki number ka hamai factors find
+ * karana hai
+ *
+ *
+ */
+class DisplayFactorsOfNumber {
+
+    static void factorsOfNumber(int number) {
+
+        for (int divisor = 1; divisor <= number; divisor++) {
+            if (number % divisor == 0) {
+                System.out.println("fa ctors of a number is:" + divisor);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        scanner.close();
+        factorsOfNumber(number);
+    }
+}
